ec1fca41c5604f7f3f2a6e0088b88893f4e6b0b9
Borisov Danil <danilborisov039@gmail.com>
2026-01-21 14:31:41 +0900

    completed task 5

--- Labs/lab_1.js
+++ Labs/lab_1.js
@@ -12,6 +12,7 @@ console.log(myInfoText);
 console.log(`Длина строки: ${myInfoText.length} символа`);
 console.log(`Первый символ: '${myInfoText[0]}'\nПоследний символ: '${myInfoText[myInfoText.length - 1]}'`);
 
+
 let userName = prompt("Как вас зовут?");
 let userAge = prompt("Сколько вам лет?");
 
@@ -19,3 +20,14 @@ userName = userName.trim().toLowerCase()
 userAge = Number(userAge.trim());
 
 alert(`Вас зовут ${userName} и вам ${userAge} лет`);
+
+
+let userSrting = prompt("Введите текст для обрезки");
+const startSliceIndex = Number(prompt("Введите индекс, с которого нужно начать обрезку строки"));
+const endSliceIndex = Number(prompt("Введите индекс, которым нужно закончить обрезать строки"));
+
+userSrting = userSrting.trim();
+
+const string = userSrting.slice(startSliceIndex, endSliceIndex);
+
+alert(`Результат: ${string}`);
