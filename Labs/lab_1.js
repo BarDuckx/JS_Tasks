f889881a372cc12d431eb89dd7bbe712579f86d7
Borisov Danil <danilborisov039@gmail.com>
2026-01-21 13:41:39 +0900

    completed task 1

---
+++ Labs/lab_1.js
@@ -0,0 +1,7 @@
+const myName = "Данил";
+const programmingLanguage = "JavaScript";
+const courseCreatorName = "Чеботов Н.М.";
+const reasonText = "JavaScript - самый лучший язык программирования";
+const numberOfMonths = 2;
+
+console.log(`Всем привет! Меня зовут, ${myName}. Сейчас я изучаю язык программирования ${programmingLanguage} на курсе по ${programmingLanguage} у ${courseCreatorName}.\n\nЯ хочу стать веб-разработчиком, потому что ${reasonText}. До этого я изучал ${programmingLanguage} ${numberOfMonths} месяцев. Я уверен, что пройду данный курс до конца!`);
