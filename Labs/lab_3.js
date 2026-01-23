7618905d616ca92c69d49080b45a828e8bc1568c
Borisov Danil <danilborisov039@gmail.com>
2026-01-23 17:25:02 +0900

    completed task 2

--- Labs/lab_3.js
+++ Labs/lab_3.js
@@ -11,3 +11,26 @@ const getName3 = name => `Имя равно ${name}`
 console.log(getName1('Данил'));
 console.log(getName2('Данил'));
 console.log(getName3('Данил'));
+
+
+const getSumOfNumbers = (number, type) => {
+    let sum = 0;
+    for (let i = 1; i <= number; i++) {
+        if (type === 'even') {
+            if (i % 2 === 0) {
+                sum += i;
+            }
+        } else if (type === 'odd') {
+            if (i % 2 !== 0) {
+                sum += i;
+            }
+        } else {
+            sum += i;
+        }
+    }
+    return sum;
+}
+
+console.log(getSumOfNumbers(10, 'odd'));
+console.log(getSumOfNumbers(10, 'even'));
+console.log(getSumOfNumbers(10));
