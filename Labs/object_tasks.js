58bcfe7a8c9e7ea09a94b480d739bbb0eb03e6af
Borisov Danil <danilborisov039@gmail.com>
2026-02-17 15:25:25 +0900

    completed task 3

--- Labs/object_tasks.js
+++ Labs/object_tasks.js
@@ -40,3 +40,30 @@ const giveTalonsInOrder = (patients, orders) => {
 
 const result = giveTalonsInOrder(people, ordersArr);
 console.log('result', result);
+
+
+const student = {
+    name: 'Maxim',
+    programingLanguage: 'JavaScript',
+}
+
+const handleObjects = (obj, key, action) => {
+    if (action === 'get') {
+        return obj[key];
+    } 
+    else if (action === 'add') {
+        obj[key] = '';
+        return obj;
+    } 
+    else if (action === 'delete') {
+        delete obj[key];
+        return obj;
+    } 
+    else {
+        return obj;
+    }
+}
+
+const result1 = handleObjects (student, 'programingLanguage', 'delete');
+
+console.log('result1', result1);
