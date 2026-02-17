2bf32d78a5cb3d2c392fed5d3d5fe0a19e22fc2b
Borisov Danil <danilborisov039@gmail.com>
2026-02-17 15:34:33 +0900

    completed task 4

--- Labs/object_tasks.js
+++ Labs/object_tasks.js
@@ -67,3 +67,19 @@ const handleObjects = (obj, key, action) => {
 const result1 = handleObjects (student, 'programingLanguage', 'delete');
 
 console.log('result1', result1);
+
+
+const student1 = {
+    fullName: 'Максим',
+    experienceInMonths: 12,
+    stack: ['HTML', 'CSS', 'JavaScript', 'React'],
+}
+
+giveJobToStudent = (student, jobName) => {
+    student.job = jobName;
+    alert(`Поздравляем! У студента ${student.fullName} появила новая работа! Теперь он ${student.job}`);
+    return student;
+}
+
+const updatedStudent = giveJobToStudent(student1, 'веб-разработчик');
+console.log('updatedStudent', updatedStudent);
